13dd98e823bf1296272df1bdfe0f7deb9b02a4d1
hamza-111 <abbassihamza2014@gmail.com>
2023-11-11 14:29:18 +0100

    tracked file

---
+++ components/cars/listingCar.vue
@@ -0,0 +1,21 @@
+<script setup>
+    const props = defineProps({
+        listing : Object
+    })
+    
+</script>
+<template>
+    <div class="shadow rounded flex justify-between mb-4 overflow-hidden">
+        <div class="flex">
+            <img :src="listing.url" alt="" class="w-80 h-44 mr-3">
+            <div class="p-3">
+                <h1 class="text-2xl">{{ listing.name }}</h1>
+                <p class="text-blue-400">${{ listing.price }}</p>
+            </div>
+        </div>
+        <div class="p-3 flex">
+            <NuxtLink :to="`/profile/listings/view/${listing.id}`">view</NuxtLink>
+            <p class="cursor-pointer text-red-400">Delete</p>
+        </div>
+    </div>
+</template>
--- pages/profile/listings/index.vue
+++ pages/profile/listings/index.vue
@@ -1,18 +1,14 @@
 <script setup>
-definePageMeta({
-  layout: "custom",
-});
-
-const { listings } = useCars();
+    definePageMeta({
+        layout : 'custom'
+    })
+    const {listings} = useCars()
 </script>
-
 <template>
-  <div>
-    <div class="flex justify-between mt-24 items-center">
-      <h1 class="text-6xl">My Listings</h1>
-      <NuxtLink
-        to="/profile/listings/create"
-        class="
+    <div class="flex justify-between items-center mt-24">
+
+        <h1 class="text-6xl">My listings</h1>
+        <NuxtLink to="/profile/listings/create" class="
           w-9
           h-9
           bg-blue-400
@@ -23,16 +19,9 @@ const { listings } = useCars();
           text-white
           font-bold
           cursor-pointer
-        "
-        >+</NuxtLink
-      >
+        ">+</NuxtLink>
+        <div class="shadow rounded p-3 mt-5">
+            <listingCar v-for="listing in listings" :key="listing.id" :listing="listing" />
+        </div>
     </div>
-    <div class="shadow rounded p-3 mt-5">
-      <CarListingCard
-        v-for="listing in listings"
-        :key="listing.id"
-        :listing="listing"
-      />
-    </div>
-  </div>
 </template>
